690eaa25cf450aa54454f48400e9f6d164b8a86d
uleceadiaz <101406226+uleceadiaz@users.noreply.github.com>
2022-05-06 14:24:40 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("pozik")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("biotza")
+})
+radio.onReceivedString(function (receivedString) {
+    if ("pozik" == receivedString) {
+        basic.showIcon(IconNames.Happy)
+    }
+    if ("triste" == receivedString) {
+        basic.showIcon(IconNames.Sad)
+    }
+    if ("biotza" == receivedString) {
+        basic.showIcon(IconNames.Heart)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("triste")
+})
+radio.setGroup(15)
+basic.forever(function () {
+	
+})
